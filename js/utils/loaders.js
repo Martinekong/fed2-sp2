8eb562cfe1101a8535ec45cb7273df67fd3fc560
Martinekong <martinekongsrud@outlook.com>
2025-09-12 15:55:54 +0200

    JSDoc for loaders js

--- js/utils/loaders.js
+++ js/utils/loaders.js
@@ -1,3 +1,8 @@
+/**
+ * Creates a loading spinner styled with the orange tertiary color.
+ *
+ * @returns {HTMLDivElement} - A div element with the loading spinner.
+ */
 function createLoadingSpinner() {
   const spinnerDiv = document.createElement('div');
   spinnerDiv.dataset.spinner = 'true';
@@ -11,11 +16,21 @@ function createLoadingSpinner() {
   return spinnerDiv;
 }
 
+/**
+ * Shows a loading spinner in the container provided.
+ *
+ * @param {HTMLElement} container - The container that should append the loading spinner.
+ */
 export function showLoadingSpinner(container) {
   container.setAttribute('aria-busy', 'true');
   container.append(createLoadingSpinner());
 }
 
+/**
+ * Hides the loading spinner from the container provided.
+ *
+ * @param {HTMLElement} container  - The container that the loading spinner should be removed from.
+ */
 export function hideLoadingSpinner(container) {
   container.removeAttribute('aria-busy');
   const spinner = container.querySelector('[data-spinner="true"]');
